c3d31ff4e76d290ebc1913d88547343d5df92152
Oscar A. Peña C <subtitular@gmail.com>
2023-06-27 07:34:13 -0500

    Se agrega canvas, se intenta dibujar un punto

--- src/poli/Assets/MainViewController.java
+++ src/poli/Assets/MainViewController.java
@@ -4,9 +4,19 @@
  */
 package poli.Assets;
 
+import javafx.scene.paint.Color;
+import javafx.scene.canvas.Canvas;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.Pane;
+import javafx.util.Duration;
 
 /**
  * FXML Controller class
@@ -15,12 +25,64 @@ import javafx.fxml.Initializable;
  */
 public class MainViewController implements Initializable {
 
+    @FXML
+    private Canvas canvas;
+
+    private static final int WIDTH = 400;
+    private static final int HEIGHT = 400;
+
+    private static final int RADIUS = 100;
+    private static final double ANGLE_INCREMENT = 0.05;
+
+    
+    private double centerX;
+    private double centerY;
+    private double angle;
+    private GraphicsContext gc;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        // TODO 
+        canvas = new Canvas(WIDTH, HEIGHT);
+        gc = canvas.getGraphicsContext2D();
+
+        Pane root = new Pane(canvas);
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
+
+/*        primaryStage.setTitle("Circle Animation");
+        primaryStage.setScene(scene);
+        primaryStage.show();*/
+
+        centerX = WIDTH / 2.0;
+        centerY = HEIGHT / 2.0;
+        angle = 0.0;
+
+        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(16), event -> {
+        update();
+        draw();
+        }));
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+    }
+    
+    private void update() {
+        angle += ANGLE_INCREMENT;
+        if (angle > 2 * Math.PI) {
+          angle -= 2 * Math.PI;
+        }
+    }
+    
+    private void draw() {
+        gc.clearRect(0, 0, WIDTH, HEIGHT);
+
+        double x = centerX + RADIUS * Math.cos(angle);
+        double y = centerY + RADIUS * Math.sin(angle);
+
+        gc.setFill(Color.RED);
+        gc.fillOval(x - 5, y - 5, 10, 10);
+    }
+
     
 }
