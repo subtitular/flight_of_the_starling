1246d55a2b7774615d8970ee61cff20b88e9ddd4
Oscar A. Peña C <subtitular@gmail.com>
2023-06-27 08:36:42 -0500

    Se agrega canvas, se estaba utilizando la vista en Assets

--- src/poli/Assets/MainViewController.java
+++ src/poli/Assets/MainViewController.java
@@ -28,61 +28,44 @@ public class MainViewController implements Initializable {
     @FXML
     private Canvas canvas;
 
-    private static final int WIDTH = 400;
-    private static final int HEIGHT = 400;
-
-    private static final int RADIUS = 100;
-    private static final double ANGLE_INCREMENT = 0.05;
-
-    
     private double centerX;
     private double centerY;
     private double angle;
-    private GraphicsContext gc;
+
     /**
      * Initializes the controller class.
      */
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO 
-        canvas = new Canvas(WIDTH, HEIGHT);
-        gc = canvas.getGraphicsContext2D();
-
-        Pane root = new Pane(canvas);
-        Scene scene = new Scene(root, WIDTH, HEIGHT);
-
-/*        primaryStage.setTitle("Circle Animation");
-        primaryStage.setScene(scene);
-        primaryStage.show();*/
-
-        centerX = WIDTH / 2.0;
-        centerY = HEIGHT / 2.0;
+    public void initialize(URL url, ResourceBundle rb){
+        /*centerX = canvas.getWidth() / 2.0;
+        centerY = canvas.getHeight() / 2.0;
         angle = 0.0;
 
+        // Crear la animación para actualizar el punto en cada fotograma
         Timeline timeline = new Timeline(new KeyFrame(Duration.millis(16), event -> {
-        update();
-        draw();
+            update();
+            draw();
         }));
         timeline.setCycleCount(Animation.INDEFINITE);
-        timeline.play();
+        timeline.play();*/
     }
-    
+
     private void update() {
-        angle += ANGLE_INCREMENT;
+        angle += 0.05;
         if (angle > 2 * Math.PI) {
-          angle -= 2 * Math.PI;
+            angle -= 2 * Math.PI;
         }
     }
-    
-    private void draw() {
-        gc.clearRect(0, 0, WIDTH, HEIGHT);
 
-        double x = centerX + RADIUS * Math.cos(angle);
-        double y = centerY + RADIUS * Math.sin(angle);
+    private void draw() {
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
+        double radius = Math.min(canvas.getWidth(), canvas.getHeight()) / 2.0 - 5;
+        double x = centerX + radius * Math.cos(angle);
+        double y = centerY + radius * Math.sin(angle);
 
         gc.setFill(Color.RED);
         gc.fillOval(x - 5, y - 5, 10, 10);
     }
-
-    
 }
--- src/poli/Views/MainView.java
+++ src/poli/Views/MainView.java
@@ -7,9 +7,13 @@ package poli.Views;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -17,12 +21,22 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.util.Duration;
 /**
  *
  * @author subti
  */
 public class MainView extends Application implements Initializable {
+    @FXML
+    private Canvas canvas;
+    
+    private double centerX;
+    private double centerY;
+    private double angle;
     
     @Override
     public void start(Stage primaryStage) throws IOException {
@@ -31,7 +45,7 @@ public class MainView extends Application implements Initializable {
 
         Scene scene = new Scene(root, 800, 600);
         primaryStage.setMaximized(true);
-        primaryStage.setFullScreen(true);
+        //primaryStage.setFullScreen(true);
         primaryStage.setScene(scene);
         primaryStage.setTitle("El Vuelo de los Estorninos");
         
@@ -43,7 +57,7 @@ public class MainView extends Application implements Initializable {
         // Configurar el escenario principal (Stage)
         primaryStage.setScene(scene);
         primaryStage.setTitle("El Vuelo de los Estorninos");
-        primaryStage.setResizable(false);
+        primaryStage.setResizable(true);
 
         // Mostrar la ventana principal
         primaryStage.show();
@@ -77,7 +91,36 @@ public class MainView extends Application implements Initializable {
     }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }   
+        centerX = canvas.getWidth() / 2.0;
+        centerY = canvas.getHeight() / 2.0;
+        angle = 0.0;
+
+        // Crear la animación para actualizar el punto en cada fotograma
+        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(16), event -> {
+            update();
+            draw();
+        }));
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+    }
+
+    private void update() {
+        angle += 0.05;
+        if (angle > 2 * Math.PI) {
+            angle -= 2 * Math.PI;
+        }
+    }
+
+    private void draw() {
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
+        double radius = Math.min(canvas.getWidth(), canvas.getHeight()) / 2.0 - 5;
+        double x = centerX + radius * Math.cos(angle);
+        double y = centerY + radius * Math.sin(angle);
+
+        gc.setFill(Color.RED);
+        gc.fillOval(x - 5, y - 5, 10, 10);
+    }
     
 }
