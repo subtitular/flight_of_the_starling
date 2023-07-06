acfc231c7c1f15ec976e13832dc9cd155720195e
Oscar A. Peña C <subtitular@gmail.com>
2023-07-05 20:02:17 -0500

    Agrego clase FlyingPath para tratar de reproducir vuelo mas organico

---
+++ src/poli/Models/FlyingPath.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poli.Models;
+
+/**
+ *
+ * @author subti
+ */
+public class FlyingPath {
+    
+}
